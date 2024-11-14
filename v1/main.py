236950c37e4e2a6c593c8ff1d606cff69ad9726c
raihannurhidayat <grandrx041@gmail.com>
2024-11-14 22:12:52 +0700

    feat: add screen game over

--- v1/main.py
+++ v1/main.py
@@ -18,6 +18,7 @@ class Game:
         self.enemy_spritesheet = Spritesheet("img/enemy.png")
 
         self.intro_background = pygame.image.load("./img/introbackground.png")
+        self.go_background = pygame.image.load("./img/gameover.png")
 
     def createTileMap(self):
         for i, row in enumerate(tilemap):
@@ -63,10 +64,36 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        self.running = False
+        # self.running = False
 
     def game_over(self):
-        pass
+        text = self.font.render("Game Over", True, WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
+
+        restart_button = Button(
+            10, WIN_HEIGHT - 60, 120, 50, WHITE, BLACK, "Restart", 24
+        )
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.isPressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go_background, (0, 0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
     def intro_screen(self):
         intro = True
