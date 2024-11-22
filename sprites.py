8c6c9403dc2f68a50d5b54e21c65afb339ac0f3c
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2024-11-22 11:40:37 +0700

    fix(player): fix player layer group variable name

--- sprites.py
+++ sprites.py
@@ -8,7 +8,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.game = game
         self._layer = PLAYER_LAYER
-        self.groups = self.game.all_sprite
+        self.groups = self.game.all_sprites
 
         pygame.sprite.Sprite.__init__(self, self.groups)
 
