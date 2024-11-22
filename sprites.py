458735f85818a59236de72dfcc95e359e37e2be4
Rosi Saraswati Alrasid <237006087@student.unsil.ac.id>
2024-11-22 16:10:57 +0700

    add movement

--- sprites.py
+++ sprites.py
@@ -17,6 +17,11 @@ class Player(pygame.sprite.Sprite):
         self.width = TILE_SIZE
         self.height = TILE_SIZE
 
+        self.x_change = 0
+        self.y_change = 0
+
+        self.facing = 'down'
+
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill(COLOR_RED)
 
@@ -25,4 +30,23 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = self.y
 
     def update(self):
-        pass
+        self.movement()
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
+        self.x_change = 0
+        self.y_change = 0
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_change -= PLAYER_SPEED
+            self.facing = 'left'
+        if keys[pygame.K_RIGHT]:
+            self.x_change += PLAYER_SPEED
+            self.facing = 'right'
+        if keys[pygame.K_UP]:
+            self.y_change -= PLAYER_SPEED
+            self.facing = 'up'
+        if keys[pygame.K_DOWN]:
+            self.y_change += PLAYER_SPEED
+            self.facing = 'down'
