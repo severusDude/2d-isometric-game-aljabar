a4417c9eda8fe4488b1b771ba13fec40bcae009e
Rosi Saraswati Alrasid <237006087@student.unsil.ac.id>
2024-11-29 00:31:34 +0700

    Perubahan camera

--- sprites.py
+++ sprites.py
@@ -53,15 +53,23 @@ class Player(pygame.sprite.Sprite):
     def movement(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            for sprite in self.game.all_sprites:
+                sprite.react.x += PLAYER_SPEED
             self.x_change -= PLAYER_SPEED
             self.facing = 'left'
         if keys[pygame.K_RIGHT]:
+            for sprite in self.game.all_sprites:
+                sprite.react.x += PLAYER_SPEED
             self.x_change += PLAYER_SPEED
             self.facing = 'right'
         if keys[pygame.K_UP]:
+            for sprite in self.game.all_sprites:
+                sprite.react.y += PLAYER_SPEED
             self.y_change -= PLAYER_SPEED
             self.facing = 'up'
         if keys[pygame.K_DOWN]:
+            for sprite in self.game.all_sprites:
+                sprite.react.y += PLAYER_SPEED
             self.y_change += PLAYER_SPEED
             self.facing = 'down'
 
