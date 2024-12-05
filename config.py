be99982f584bd77b39d69ac6d0416371d58efd48
studentsinformatics23 <htullah321@gmail.com>
2024-12-05 14:22:22 +0700

    Fix Menu

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 SPRITE_CHARACTER_FILE = R"img/character.png"
 SPRITE_TERRAIN_FILE = R"img/terrain.png"
+SPRITE_ENEMY_FILE = R"img/enemy.png"
 
 SPRITE_BLOCK_COORD = (960, 448)
 SPRITE_TERRAIN_COORD = (64, 352)
@@ -17,18 +18,22 @@ WIN_HEIGHT = 480
 TILE_SIZE = 32
 FPS = 60
 
-PLAYER_LAYER = 3
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
 BLOCK_LAYER = 2
 GROUND_LAYER = 1
+
 PLAYER_SPEED = 3
+ENEMY_SPEED = 2
 
 COLOR_RED = (255, 0, 0)
 COLOR_BLACK = (0, 0, 0)
 COLOR_BLUE = (0, 0, 255)
+COLOR_WHITE = (255, 255, 255)
 
 tilemap = [
     "BBBBBBBBBBBBBBBBBBBB",
-    "B..................B",
+    "B..E...............B",
     "B..................B",
     "B...BBB............B",
     "B..................B",
@@ -38,7 +43,7 @@ tilemap = [
     "B.....BB...........B",
     "B......B...........B",
     "B......B...........B",
-    "B......B...........B",
+    "B......B......E....B",
     "B..................B",
     "B..................B",
     "BBBBBBBBBBBBBBBBBBBB",
--- main.py
+++ main.py
@@ -9,12 +9,14 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
-        # self.font = pygame.font.Font('Arial', 32)
+        self.font = pygame.font.Font(size=32)
         self.running = True
 
         self.character_spritesheet = Spritesheet(SPRITE_CHARACTER_FILE)
         self.terrain_spritesheet = Spritesheet(SPRITE_TERRAIN_FILE)
-
+        self.enemy_spritesheet = Spritesheet(SPRITE_ENEMY_FILE)
+        self.intro_background = pygame.image.load("img/introbackground.png")
+        self.go_background = pygame.image.load("img/gameover.png")
         self.attack_spritesheet = Spritesheet("img/attack.png")
 
     def createTileMap(self):
@@ -23,7 +25,9 @@ class Game:
                 Ground(self, j, i)
                 if column == "B":
                     Blocks(self, j, i)
-                if column == "P":
+                if column == "E":
+                    Enemy(self, j, i)
+                if column == "P":  
                     self.player = Player(self, j, i)
 
     def new(self):
@@ -72,13 +76,60 @@ class Game:
             self.update()
             self.draw()
 
-        self.running = False
+
 
     def game_over(self):
-        pass
+        text = self.font.render('Game Over', True, COLOR_WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        restart_button = Button(10, WIN_HEIGHT - 60, 120, 50, COLOR_WHITE, COLOR_BLACK, 'Restart Game', 32)
+
+        for sprite in self.all_sprites:  
+            sprite.kill()
+        
+        while self.running:
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go_background, (0,0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('Great Game', True, COLOR_BLACK)
+        title_rect = title.get_rect(x=100, y=100)
+
+        play_button = Button(100, 50, 100, 50, COLOR_WHITE, COLOR_BLACK, 'PLAY', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
 
 if __name__ == "__main__":
@@ -88,6 +139,7 @@ if __name__ == "__main__":
 
     while g.running:
         g.main()
+        g.game_over()
 
     pygame.quit()
     sys.exit()
